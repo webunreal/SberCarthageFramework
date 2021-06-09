6d1efeb48b432b247400a21b97b8312a3ff16b23
Nikolai Sokol <nikolaisokol@icloud.com>
2021-06-09 13:39:51 +0300

    yep

---
+++ SberCarthageFramework/Something.swift
@@ -0,0 +1,14 @@
+//
+//  Something.swift
+//  SberCarthageFramework
+//
+//  Created by Nikolai Sokol on 09.06.2021.
+//
+
+import Foundation
+
+class Something {
+    func makeSomething() {
+        print("Oh! That works")
+    }
+}
